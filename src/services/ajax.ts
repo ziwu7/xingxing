130f8b261c2f14bc2c30ef512c0e632990aaeced
ziwu7 <263377135@qq.com>
2023-09-07 14:48:11 +0800

    axios拦截器初步处理后台数据

---
+++ src/services/ajax.ts
@@ -0,0 +1,26 @@
+import { message } from "antd";
+import axios from "axios";
+const instance = axios.create({ timeout: 10 * 1000 });
+
+instance.interceptors.response.use((res) => {
+  const resData = (res.data || {}) as ResType;
+  const { errno, data, msg } = resData;
+  if (errno !== 0) {
+    if (msg) {
+      message.error(msg);
+    }
+    throw new Error(msg);
+  }
+  return data as any;
+});
+
+export default instance;
+
+export type ResType = {
+  errno: number;
+  data?: ResDataType;
+  msg?: string;
+};
+export type ResDataType = {
+  [key: string]: any;
+};
