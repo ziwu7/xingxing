b1c22e042dcbeed1233ba50ce3cac63e6bcc8fb5
ziwu7 <263377135@qq.com>
2023-09-12 16:02:10 +0800

    增加加载更多功能（麻烦）

--- src/pages/manage/List.tsx
+++ src/pages/manage/List.tsx
@@ -1,11 +1,18 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState, useRef, useMemo } from "react";
 import QuestionCard from "../../components/QuestionCard";
 import styles from "./common.module.scss";
 // import { produce } from "immer";
-import { Typography, Spin } from "antd";
-import { useRequest, useTitle } from "ahooks";
+import { Typography, Spin, Empty } from "antd";
+import { useDebounceFn, useRequest, useTitle } from "ahooks";
 import ListSearch from "../../components/ListSearch";
 import useLoadQuestionListData from "../../hooks/useLoadQuestionListData";
+import { useParams, useSearchParams } from "react-router-dom";
+import { getQuestionListService } from "../../services/question";
+import {
+  LIST_PAGE_PARM_KEY,
+  LIST_PAGE_SIZE,
+  LIST_SEARCH_PARM_KEY,
+} from "../../constant";
 // const rawQuestionList = [
 //   {
 //     _id: "q1", //_id用这个是因为后端moogodb用的这个，方便统一
@@ -33,12 +40,83 @@ import useLoadQuestionListData from "../../hooks/useLoadQuestionListData";
 //   },
 // ];
 const List: FC = () => {
-  const { data = {}, loading, error } = useLoadQuestionListData();
-  const { list = [], total = 0 } = data;
-
-  // const [questionList, setquestionList] = useState(rawQuestionList);
-  const { Title } = Typography;
+  // const { data = {}, loading, error } = useLoadQuestionListData();
+  // const { list = [], total = 0 } = data;
   useTitle("我的问卷");
+  // const [questionList, setquestionList] = useState(rawQuestionList);
+  const [started, setStarted] = useState(false); //是否已加载，（防抖有延时时间，优化显示交互）
+  const [page, setPage] = useState(1);
+  const [list, setList] = useState([]); //上滑加载更多是，累计显示的总数据量
+  const [total, setTotal] = useState(0);
+  const haveMoreData = total > list.length; //总数据量判断
+  const { Title } = Typography;
+  const [searchParams] = useSearchParams();
+  const keyword = searchParams.get(LIST_SEARCH_PARM_KEY) || "";
+
+  //监测keyword变化时重置信息
+  useEffect(() => {
+    setPage(1);
+    setList([]);
+    setTotal(0);
+  }, [keyword]);
+  //执行获取数据，真正要加载的
+  const { run: load, loading } = useRequest(
+    async () => {
+      const data = await getQuestionListService({
+        page,
+        pageSize: LIST_PAGE_SIZE,
+        keyword,
+      });
+      return data;
+    },
+    {
+      manual: true,
+      onSuccess(result) {
+        const { list: l = [], total = 0 } = result;
+        setList(list.concat(l));
+        setTotal(total);
+        setPage(page + 1);
+      },
+    },
+  );
+
+  //触发执行tryLoadMore，防抖，定义防抖函数为tryLoadMore,加载更多 主函数
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { run: tryLoadMore } = useDebounceFn(
+    () => {
+      const elem = containerRef.current;
+      if (elem == null) return;
+      const domRect = elem.getBoundingClientRect();
+      if (domRect == null) return;
+      const { bottom } = domRect;
+      console.log(bottom);
+      console.log(document.documentElement.clientHeight);
+      //当loadMore显示出来时bottom <= document.documentElement.clientHeight，重要，要用document.documentElement.clientHeight， 不能是document.body.clientHeight
+      if (bottom <= document.documentElement.clientHeight) {
+        console.log("loadmore!!!");
+        load(); //真正获取数据
+        setStarted(true);
+      }
+    },
+    { wait: 1000 },
+  );
+
+  //1.组件刚渲染时加载，或url变化时加载（keyword变化时重新加载
+  useEffect(() => {
+    tryLoadMore();
+  }, [searchParams]);
+  //2.滚动到底部时也加载
+  useEffect(() => {
+    console.log(haveMoreData);
+
+    if (haveMoreData) {
+      window.addEventListener("scroll", tryLoadMore); //防抖
+    }
+
+    return () => {
+      window.removeEventListener("scroll", tryLoadMore); //解绑事件
+    };
+  }, [searchParams, haveMoreData]); //URL变化时依然重新监听滚动事件,首次加载时更新haveMoreData
   // const add = () => {
   //   const newwen = {
   //     id: "q4" + Math.random().toString().slice(-3),
@@ -76,8 +154,16 @@ const List: FC = () => {
   //     }),
   //   );
   // };
+
+  const LoadMoreContentElem = useMemo(() => {
+    if (!started || loading) return <Spin />;
+    if (total === 0) return <Empty description="暂无数据" />;
+    if (!haveMoreData) return <span>没有更多...</span>;
+    return <span>开始加载下一页</span>;
+  }, [loading, haveMoreData]);
+
   return (
-    <div>
+    <>
       <div className={styles.header}>
         <div className={styles.left}>
           <Title level={3}>我的问卷</Title>
@@ -87,17 +173,19 @@ const List: FC = () => {
         </div>
       </div>
       <div className={styles.content}>
-        <div style={{ textAlign: "center" }}>{loading && <Spin />}</div>
-        {!loading &&
-          list.length > 0 &&
+        {/* <div style={{ height: "2000px" }}></div> */}
+        {/* <div style={{ textAlign: "center" }}>{loading && <Spin />}</div> */}
+        {list.length > 0 &&
           list.map((question: any) => {
             const { _id } = question;
             return <QuestionCard key={_id} {...question} />;
           })}
         {/* <button onClick={add}>新增问卷</button> */}
       </div>
-      <div className={styles.footer}>load more</div>
-    </div>
+      <div className={styles.footer}>
+        <div ref={containerRef}> {LoadMoreContentElem}</div>
+      </div>
+    </>
   );
 };
 
