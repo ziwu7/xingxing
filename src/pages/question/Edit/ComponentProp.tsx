1dde8cb15d7b77ec31a354441c89a184994defe1
ziwu7 <263377135@qq.com>
2023-11-18 17:32:41 +0800

    在组件的属性面板将变化的属性更新到redux中

--- src/pages/question/Edit/ComponentProp.tsx
+++ src/pages/question/Edit/ComponentProp.tsx
@@ -1,18 +1,31 @@
 import React, { FC } from "react";
 import useGetComponentInfo from "../../../hooks/useGetComponentInfo";
-import { getComponentConfByType } from "../../../components/QuestionComponents";
+import {
+  ComponentPropsType,
+  getComponentConfByType,
+} from "../../../components/QuestionComponents";
+import { useDispatch } from "react-redux";
+import { changeComponentProps } from "../../../store/componentsReducer";
 const NoProp: FC = () => {
   return <div>未选中组件</div>;
 };
 //根据画布选中的组件，获取其type，props，根据type找到组件配置，进而解析并生成属性组件PropComponent
 const ComponentProp: FC = () => {
+  const dispatch = useDispatch();
   const { selectedComponent } = useGetComponentInfo();
   if (selectedComponent == null) return <NoProp />;
   const { type, props } = selectedComponent;
   const componentConf = getComponentConfByType(type);
   if (componentConf == null) return <NoProp />;
   const { PropComponent } = componentConf;
-  return <PropComponent {...props} />;
+  function changeProps(newProps: ComponentPropsType) {
+    console.log("newProps", newProps);
+    if (selectedComponent == null) return;
+    const { fe_id } = selectedComponent;
+    dispatch(changeComponentProps({ fe_id, newProps }));
+  }
+
+  return <PropComponent {...props} onChange={changeProps} />;
 };
 
 export default ComponentProp;
