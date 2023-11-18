2901be62b3e835a7170ab36347c0547fa1ec557c
ziwu7 <263377135@qq.com>
2023-11-18 17:30:18 +0800

    给组件添加onChange监听数据变化，注意：antd的form表单通过onValuesChange来监听数据变化

--- src/components/QuestionComponents/QuestionInput/PropComponent.tsx
+++ src/components/QuestionComponents/QuestionInput/PropComponent.tsx
@@ -5,13 +5,25 @@ import { Form, Input } from "antd";
 const PropComponent: FC<QuestionInputPropsType> = (
   props: QuestionInputPropsType,
 ) => {
-  const { title, placeholder } = props;
+  const { title, placeholder, onChange } = props;
   const [form] = Form.useForm();
   useEffect(() => {
     form.setFieldsValue({ title, placeholder });
   }, [title, placeholder]);
+
+  function handleValueChange() {
+    if (onChange) {
+      onChange(form.getFieldsValue());
+    }
+  }
+
   return (
-    <Form layout="vertical" initialValues={{ title, placeholder }} form={form}>
+    <Form
+      layout="vertical"
+      initialValues={{ title, placeholder }}
+      form={form}
+      onValuesChange={handleValueChange}
+    >
       <Form.Item
         label="标题"
         name="title"
